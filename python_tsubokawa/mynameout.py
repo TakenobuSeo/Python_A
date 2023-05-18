7fcf79f8291e3cac976960813bddf0f0fb803ad9
yoichi-tsubo <tsubokawa.yoichi.ol@mynavi.jp>
2023-05-18 04:17:39 +0000

    問1の回答です

---
+++ python_tsubokawa/mynameout.py
@@ -0,0 +1,10 @@
+import sys
+args = sys.argv
+
+name = args[1]
+num = 30
+
+# message = "Hello " + name
+# print("Hello, \"World !", end="")
+print('Hello '+ name +' !')
+print('Hello '+ str(num) +' !')

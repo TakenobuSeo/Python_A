87c909459e1522316daef9a934339c11b207ca82
yoichi-tsubo <tsubokawa.yoichi.ol@mynavi.jp>
2023-05-19 06:07:47 +0000

    コメントを追加

--- python_tsubokawa/distance.py
+++ python_tsubokawa/distance.py
@@ -19,10 +19,13 @@ args = sys.argv
 departure_station = args[1]
 arrival_station = args[2]
 
-
+#距離を辞書から選択
 departure_distance = distance_from_Tokyo[departure_station]
 arrival_distance = distance_from_Tokyo[arrival_station]
 
+#2駅間の距離を計算
+#負の場合でも良いように絶対値計算
 distance_between = abs(arrival_distance - departure_distance)
 
+#少数2桁で丸めて出力
 print(round(distance_between,2), end="")

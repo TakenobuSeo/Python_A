548d212d7949cdbe1891e461d66bb24af95d31fb
yoichi-tsubo <tsubokawa.yoichi.ol@mynavi.jp>
2023-05-19 05:14:48 +0000

    入力値エラー処理を追加

--- python_tsubokawa/population.py
+++ python_tsubokawa/population.py
@@ -1,11 +1,18 @@
+
+#国のタプル作成
 countries = ("China", "India", "U.S.A", "Indonesia",
   "Pakistan", "Brazil", "Nigeria",
     "Bangladesh", "Russia", "Mexico")
 
 # #入力値の取得
-import sys
-args = sys.argv
+# import sys
+# args = sys.argv
 
-index = int(args[1])
+# index = int(args[1])
 
-print(countries[index-1])
+#デバッグ
+index = 3
+if (index<=0):
+    print('エラー : 1以上の整数を入力してください')
+else:
+    print(countries[index-1], end="")

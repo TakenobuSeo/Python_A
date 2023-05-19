12d8cf0a9d840b168fee33f05209e22d851e0b4f
sanhir <sano.hirofumi.yz@mynavi.jp>
2023-05-19 06:58:24 +0000

    チャレンジ問題更新

--- python_sano/primefactor.py
+++ python_sano/primefactor.py
@@ -7,10 +7,9 @@ num = int(args[1])
 # 素因数分解して、素因数をprime_factorに格納していく
 prime_factor = []
 while num > 1:
-    for i in range(2, num+1):
+    for i in range(2, int(num)+1):
         if num % i == 0:
             num /= i
-            num = int(num)
             prime_factor.append(i)
             break
 

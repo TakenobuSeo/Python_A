98cf6aaefd16dbe458961baff569ad54a55391cf
yoichi-tsubo <tsubokawa.yoichi.ol@mynavi.jp>
2023-05-19 05:11:51 +0000

    population.pyを作成

--- python_tsubokawa/animallist.py
+++ python_tsubokawa/animallist.py
@@ -1,16 +1,16 @@
 #  "giraffe", "tiger", "zebra", "elephant", "lion"
 animal_list = ['giraffe', 'tiger', 'zebra', 'elephant', 'lion']
 
-#入力値の取得
-import sys
-args = sys.argv
+# #入力値の取得
+# import sys
+# args = sys.argv
 
-index = int(args[1])
-animal = args[2]
+# index = int(args[1])
+# animal = args[2]
 
 #デバッグ用
-# index = 3
-# animal = 'human'
+index = 3
+animal = 'human'
 
 #挿入処理
 animal_list.insert(index, animal)

7687341da0e58cd7569188951626c5e0103f5504
nakayuna27 <nakagawa.yuna.xa@mynavi.jp>
2023-05-19 02:23:35 +0000

    配列を渡す場合

--- python_nakagawa/sheep.py
+++ python_nakagawa/sheep.py
@@ -2,8 +2,9 @@
 import sys
 args = sys.argv
 
-sheep_sum = int(args[1])
+# sheep_sum = int(args[1])
+sheep_sum = [1,2,3,4]
 
 #渡されたひつじの数だけ繰り返す
-for i in range(sheep_sum):
+for i in range(len(sheep_sum)):
     print(f'ひつじが{i + 1}匹')

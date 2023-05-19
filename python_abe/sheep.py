3832927d4e8053ebc7c38b1d978fabc8e93e7f48
RyuyaAbe-dev <abe.ryuya.vo@mynavi.jp>
2023-05-19 01:49:30 +0000

    sheep.py追加

---
+++ python_abe/sheep.py
@@ -0,0 +1,5 @@
+import sys
+args = sys.argv
+n = int(args[1])
+for i in range(n):
+    print(f'ひつじが{i}匹')
---
+++ python_abe/sum1to10.py
@@ -0,0 +1,5 @@
+v = 0
+for i in range(1,11):
+    v = v+1
+    print(i, "を足すと", v)
+print("1から10をたすと",v)

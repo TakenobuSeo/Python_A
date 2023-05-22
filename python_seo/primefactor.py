b22768b87c114c799fe247a43d696dbd28528f8d
TakenobuSeo <seo.takenobu.bl@mynavi.jp>
2023-05-22 08:12:33 +0000

    primefactor.pyのアルゴリズムを作成

--- python_seo/primefactor.py
+++ python_seo/primefactor.py
@@ -4,7 +4,12 @@ args = sys.argv
 # 引数を数値型に変換
 num = int(args[1])
 
-i = 2
+sosu = []
 
-while i <= num:
-    num = num / i
+for i in range(2, num+1):
+    while num % i == 0:
+        sosu.append(i)
+        print(i)
+        num = num // i
+
+print(sosu)

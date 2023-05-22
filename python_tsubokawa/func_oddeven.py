5b841b7c4c39539b39ec73f12a0db89575729e3d
yoichi-tsubo <tsubokawa.yoichi.ol@mynavi.jp>
2023-05-22 04:57:17 +0000

    func_oddeven.pyを作成

---
+++ python_tsubokawa/func_oddeven.py
@@ -0,0 +1,22 @@
+
+from func import func as calcFunc
+
+# calcFunc = func
+#debug
+num1 = 2
+num2 = 3
+num3 = 4
+
+# #入力値の取得
+# import sys
+# args = sys.argv
+# num1 = int(args[1])
+# num2 = int(args[2])
+# num3 = int(args[3])
+
+
+calcFunc.calcvalue(num1)
+calcFunc.calcvalue(num2)
+calcFunc.calcvalue(num3)
+
+

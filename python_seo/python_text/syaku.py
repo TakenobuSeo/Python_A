86802b38d89bfb1c9263c437e1b8a9f0db951322
TakenobuSeo <seo.takenobu.bl@mynavi.jp>
2023-05-24 00:41:35 +0000

    syaku.py, test-syaku.pyにメモを追加

--- python_seo/python_text/syaku.py
+++ python_seo/python_text/syaku.py
@@ -1,2 +1,2 @@
 def syaku_to_cm(syaku):
-    return round(syaku * 30.303, 3)
+    return round(syaku * 30.303, 3)  # 小数点第3位以下をround関数で四捨五入
--- python_seo/python_text/test-syaku.py
+++ python_seo/python_text/test-syaku.py
@@ -1,6 +1,6 @@
-import syaku
+import syaku  # syaku.pyをインポート
 
-v = syaku.syaku_to_cm(10)
+v = syaku.syaku_to_cm(10)  # syaku.pyファイルのsyaku_to_cm()という関数を利用
 print("10尺=", v ,"cm")
 
 v = syaku.syaku_to_cm(20)

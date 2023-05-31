6111db53dfa027277bac5320694b7183fa7288ad
TakenobuSeo <seo.takenobu.bl@mynavi.jp>
2023-05-31 07:47:27 +0000

    tableを追加

--- python_seo/table.py
+++ python_seo/table.py
@@ -17,6 +17,31 @@ class Attendnum(Base):
     adult = Column('adult', Integer)    
     child = Column('child', Integer) 
 
+class mst_merchandise(Base):
+    __tablename__ = 'mst_merchandise'
+    id = Column('id', String(10), primary_key=True)
+    name = Column('name', String(20))
+    price = Column('price', Integer)
+     
+
+class tbl_stock(Base):
+    __tablename__ = 'tbl_stock'
+    id = Column('id', String(10), primary_key=True)
+    stock = Column('stock', Integer)
+
+class tbl_money(Base):
+    __tablename__ = 'tbl_money'
+    price = Column('price', Integer, primary_key=True)
+    number = Column('number', Integer)
+
+class tbl_message(Base):
+    __tablename__ = 'tbl_message'
+    seq = Column('seq', Integer, primary_key=True)
+    message = Column('message', String(100))
+    datetime = Column('datetime', Date)
+    
+
+
 def main(args):
     """
     メイン関数
